e56c7f98ff58f0d37f8ae7681a9cffe77fb53b03
4times <hctsh@163.com>
2018-08-15 16:22:09 +0800

    Initial commit

---
+++ 01.py
@@ -0,0 +1,13 @@
+class students():
+    age = 18
+    name = '名字'
+    gender = 1
+
+    def _money_(self):
+        if money >= 10000 :
+            print('有钱')
+        else:
+            print('穷逼')
+liuzong = students()
+print(liuzong.age)
+print(students.__dict__)
