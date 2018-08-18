48c252f34edc9ba3faeaaffb856d370bd35c0140
4times <hctsh@163.com>
2018-08-18 20:54:47 +0800

    text

--- 01.py
+++ 01.py
@@ -1,13 +1,27 @@
 class students():
-    age = 18
-    name = '名字'
-    gender = 1
-
-    def _money_(self):
-        if self >= 10000 :
-            print('有钱')
+    def __init__(self,age,gender,name,money,IQ):
+        self.age = age
+        self.gender = gender
+        self.name = name
+        self.money = money
+        self.IQ = IQ
+    def get_money(self):
+        if self.money >= 10000:
+            return '有钱人'
         else:
-            print('穷逼')
-liuzong = students()
-print(liuzong.age)
+            return '穷逼'
+    def get_IQ(self):
+        return '九九乘法表'
+
+liuzong = students(40,'刘总','男',100,1)
 print(students.__dict__)
+print(liuzong)
+print(liuzong.money)
+print(liuzong.get_money())
+print(liuzong.age)
+print(liuzong.name)
+print(liuzong.get_IQ())
+for i in range(1,10):
+    for j in range(1,i+1):
+        print('{}×{}={:<3}'.format(j,i,i*j),end='')
+    print()
